685d604e6fb7d96bfd828114d6057807530bc510
goodfortuneforyou <jay70sawaynfli@hotmail.com>
2025-09-08 01:54:51 -0500

    add new field for FadeIn component

--- src/components/motions/FadeIn.tsx
+++ src/components/motions/FadeIn.tsx
@@ -1,6 +1,7 @@
 import { motion } from "framer-motion";
 import type { PropsWithChildren } from "react";
 
+
 interface MotionProps extends PropsWithChildren {
   className?: string;
   delay?: number;
